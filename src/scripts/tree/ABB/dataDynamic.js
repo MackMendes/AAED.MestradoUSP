965017d0f32dae151e52e651d4ddfd8bc2623e82
Charles Mendes de Macedo <MackMendes@users.noreply.github.com>
2016-06-06 14:11:52 -0300

    Criação dinâmica da árvore binária
    Criação dinâmica da árvore binária

--- src/scripts/tree/ABB/dataDynamic.js
+++ src/scripts/tree/ABB/dataDynamic.js
@@ -1,1 +1,10 @@
-﻿var DataDynamic = [];
+// Nó Raiz
+var NodeBaseDynamic = null;
+
+// Classe com as estrutura de árvore utilizada no exemplo da professora Karina
+var TreeDynamic = function (info) {
+    this.info = isNaN(parseInt(info)) ? null : parseInt(info);
+    this.esq = null;
+    this.dir = null;
+    this.pai = null;
+};
