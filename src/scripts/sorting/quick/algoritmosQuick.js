3e1e63f0d9a3b391365ef532f004cdde23297848
Charles Mendes de Macedo <charlesmendes_31@hotmail.com>
2016-06-22 01:57:51 -0300

    Implementação do Algoritmo de QuickSort. E algoritmo de Quick, versão prova.

---
+++ src/scripts/sorting/quick/algoritmosQuick.js
@@ -0,0 +1,66 @@
+﻿var algorQuick = (function () {
+    var partione = function (A, p, r) {  // supõe p <= r
+        var x = A[r];
+        var i = p - 1;
+        var temp;
+        for (var j = p; j <= r - 1; j++) {
+            if (A[j] <= x) {
+                i = i + 1;
+                A = swap(A, i, j); // swap é uma função para A[i] <-> A[j];
+            };
+        }
+        A = swap(A, (i + 1), r); // swap é uma função para A[i + 1] <-> A[r] 
+        return i + 1;
+    };
+
+    var quickSort = function (A, p, r) {
+        var q;
+        if (p < r) {
+            q = partione(A, p, r);
+            console.log(A);
+            quickSort(A, p, q - 1);
+            console.log(A);
+            quickSort(A, q + 1, r);
+            console.log(A);
+        }
+    };
+
+    return {
+        QuickSort: quickSort
+    };
+
+})();
+
+
+algorQuick.Quick2 = (function () {
+    
+    var sep = function (v, p, r) {
+        var i, t;
+        for (var i = p + 1; i <= r; i++) {
+            if (v[p] > v[i]) {
+                t = v[p];
+                v[p] = v[i];
+                v[i] = t;
+            }
+
+        }
+        return p;
+    };
+
+    var quickSort = function (v, p, r) {
+        var j;
+        if (p < r) {
+            j = sep(v, p, r);
+            console.log(v);
+            quickSort(v, p, j - 1);
+            console.log(v);
+            quickSort(v, j + 1, r);
+            console.log(v);
+        }
+    };
+
+    return {
+        QuickSort: quickSort
+    };
+
+})();
---
+++ src/scripts/sorting/quick/pageHelper.js
@@ -0,0 +1,19 @@
+﻿var TesteQuickSort = function () {
+    var A = [99, 33, 55, 77, 11, 22, 88, 66, 33, 44];
+    
+    var countArray = A.length - 1;
+    algorQuick.QuickSort(A, 0, countArray);
+
+    console.log(A);
+
+};
+
+var TesteQuickSort2 = function () {
+    var A = [99, 33, 55, 77, 11, 22, 88, 66, 33, 44];
+
+    var countArray = A.length - 1;
+    algorQuick.Quick2.QuickSort(A, 0, countArray);
+
+    console.log(A);
+
+};
